961ed0353494e4642d99ac84c37a1d1233e93b3a
Yukitaka1116 <jiangyc@cndatahub.com>
2023-09-01 20:42:21 +0800

    first commit

--- newsnet/newsnet/src/main/java/com/solaluna/newsnet/mapper/NewsInfoMapper.java
+++
@@ -1,9 +0,0 @@
-package com.solaluna.newsnet.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.solaluna.newsnet.pojo.NewsInfo;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface NewsInfoMapper extends BaseMapper<NewsInfo> {
-}
